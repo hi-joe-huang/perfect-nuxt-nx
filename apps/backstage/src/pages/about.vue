0d3d6f8c96f1e94cf9641c56af23406f8062a0a9
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-06-24 12:02:12 +0800

    feat: 添加簡單計數器功能

--- apps/backstage/src/pages/about.vue
+++ apps/backstage/src/pages/about.vue
@@ -1,6 +1,32 @@
+<script setup lang="ts">
+const counter = ref(1)
+
+function handleAddCounter() {
+  counter.value++
+}
+
+function handleSubtractCounter() {
+  counter.value--
+}
+
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page.</h1>
+    
+    <div>
+      <h2>Counter</h2>
+      <button @click="handleAddCounter">
+        Add
+      </button>
+      <div>
+        {{ counter }}
+      </div>
+      <button @click="handleSubtractCounter">
+        Subtract
+      </button>
+    </div>
   </div>
 </template>
 
