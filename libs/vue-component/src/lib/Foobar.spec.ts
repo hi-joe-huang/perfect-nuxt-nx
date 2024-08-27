fdb8e5403e2f6bb219cfe9bc0cd43f7240548c37
Riceball <42161586+riceball-tw@users.noreply.github.com>
2024-08-27 14:37:02 +0800

    test: 刪除過時測試範例元件

--- libs/vue-component/src/lib/Foobar.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils';
-import Foobar from './Foobar.vue';
-
-describe('Foobar', () => {
-  it('renders properly', () => {
-    const wrapper = mount(Foobar, {});
-    expect(wrapper.text()).toContain('Welcome to Foobar');
-  });
-});
